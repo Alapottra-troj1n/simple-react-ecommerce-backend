df73d6afdb558566767f79f49d517a09fd63a1fd
Alapottra <alapottratrojon@gmail.com>
2022-04-24 00:05:29 +0600

    added products info in database and created a api endpoint to point that data

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors  = require('cors');
 const port = process.env.PORT || 5000;
 const app = express();
+require('dotenv').config(); 
 
 //middleware 
 app.use(cors());
@@ -17,12 +18,21 @@ app.get('/', (req, res) => {
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${DB_PASS}@cluster0.zf9ug.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zf9ug.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 const run = async() =>{
     try{
-        client.connect();
+      await client.connect();
+      const database = client.db('ecommerce-store');
+      const productCollection = database.collection('product-collection');
+
+      app.get('/products', async(req, res) =>{
+          const query = {};
+          const cursor = productCollection.find(query);
+          const products = await cursor.toArray();
+          res.send(products)
+      })
 
 
 
